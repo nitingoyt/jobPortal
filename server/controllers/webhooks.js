21f7677a96298c95f82609d4239882932ac5d1d9
nitingoyt <Nitingoyat99965@gmail.com>
2025-01-11 13:36:55 +0530

    minor05

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -6,21 +6,21 @@ import User from "../models/User.js";
 export const clerkWebhooks = async (req, res) => {
   try {
     // Svix instance with clerkwebhook secret
-    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+    const whook = new Webhook (process.env.CLERK_WEBHOOK_SECRET);
 
     //   verify headers
     await whook.verify(JSON.stringify(req.body), {
-      "svix-id": req.headers["svix-id"],
-      "svix-signature": req.headers["svix-signature"],
-      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-id" : req.headers["svix-id"],
+      "svix-timestamp" : req.headers["svix-timestamp"],
+      "svix-signature" : req.headers["svix-signature"],
     });
 
-    // geting data from bdy
-    const { data, type } = req.body;
+    // geting data from body
+    const { data, type } = req.body
 
     // Switch cases for diff events
     switch (type) {
-      case "user.create": {
+      case "user.created": {
         const userData = {
           _id: data.id,
           email: data.email_address[0].email_address,
--- server/models/User.js
+++ server/models/User.js
@@ -5,8 +5,8 @@ const userSchema = new mongoose.Schema({
     _id:{type:String, required:true},
     name:{type:String, required:true},
     email:{type:String, required:true, unique:true},
-    image:{type:String, default:""},
-    resume:{type:String, default:""},
+    image:{type:String},
+    resume:{type:String},
 })
 
 
